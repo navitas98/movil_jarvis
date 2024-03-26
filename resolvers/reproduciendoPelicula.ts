d4dd3c1f94184b28188667e90c3a586915fbdda3
jnavas98 <36570373+navitas98@users.noreply.github.com>
2024-03-26 11:51:57 +0100

    Update reproduciendoPelicula.ts

--- resolvers/reproduciendoPelicula.ts
+++ resolvers/reproduciendoPelicula.ts
@@ -2,9 +2,13 @@ import { Request, Response } from "npm:express@4.18.2";
 import { PeliculasModel } from "../db/Peliculas.ts";
 export const reproduciendo=async(req:Request, res:Response)=>{
     try {
-        const nombre=req.params.id;
+        
         const pelicula=await PeliculasModel.findOne({reproduciendo:true}).exec();
+        
+        
+        
        if(pelicula){
+        console.log(pelicula.nombre);
       res.status(200).send({
         id:pelicula._id.toString(),
         nombre:pelicula.nombre,
@@ -13,6 +17,8 @@ export const reproduciendo=async(req:Request, res:Response)=>{
         abierto:pelicula.abierto
       })}
       else{
+        console.log("no hay ninguna pelicula reproduciendose");
+        
         res.status(200).send(false)
       }
     } catch (error) {
